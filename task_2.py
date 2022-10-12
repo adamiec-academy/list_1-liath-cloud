b5266a4fe8ed1deecf8471bf330c1d295151299e
liath-cloud <115594751+liath-cloud@users.noreply.github.com>
2022-10-12 17:22:09 +0100

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -1,2 +1,16 @@
 def chess_board(n, k):
-    pass
+   first_pattern = (" " * k + "#" * k) * n
+   second_pattern = ("#" * k + " " * k) * n
+
+   flag = 0
+
+   for i in range(n * 2):
+
+       if flag == 0:
+           for j in range(k):
+               print(first_pattern)
+           flag = 1
+       else:
+           for j in range(k):
+               print(second_pattern)
+           flag = 0
