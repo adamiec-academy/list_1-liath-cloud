8e3f80691136b639dfdbad29a60212f2c7afb4d2
liath-cloud <115594751+liath-cloud@users.noreply.github.com>
2022-10-12 17:33:47 +0100

    Update task_5.py

--- task_5.py
+++ task_5.py
@@ -1,5 +1,18 @@
 def factorial(n):
-    pass
+   silnia = 1
+
+   if n != 0:
+
+       for i in range(1, n + 1):
+
+           silnia *= i
+
+   digits = len(str(silnia))
+   space_1 = 3 - len(str(n))
+   space_2 = 3 - len(str(digits))
+
+
+   print(" " * space_1 + str(n) + "! " + " is " + " " * space_2 + str(digits) + " digits long")
 
 
 def report():
