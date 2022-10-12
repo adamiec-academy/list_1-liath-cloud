0b4ae900bc78bf1c60bbc6f08171e71a9c04a015
liath-cloud <115594751+liath-cloud@users.noreply.github.com>
2022-10-13 00:03:57 +0100

    Update task_4.py

--- task_4.py
+++ task_4.py
@@ -1,6 +1,23 @@
+import math
+
 def snowball(n, k):
-    pass
+    radius = n / 2 # also centre coordinates
+
+    for i in range(n): # y
+        line = " " * k
+
+        for j in range(n): # x
+
+            if math.sqrt(((j + 0.5) - radius) ** 2 + ((i + 0.5) - radius) ** 2) <= radius:
+                line += "#"
+
+            else:
+                line += " "
+
+        print(line)
 
 
 def snowman(n):
-    pass
+    snowball(n, 2)
+    snowball(n+2, 1)
+    snowball(n+4, 0)
