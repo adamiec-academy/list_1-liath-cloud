950d6a697806563277dbcfce890d432b7fb79e7a
liath-cloud <115594751+liath-cloud@users.noreply.github.com>
2022-10-12 20:39:22 +0100

    Update task_5.py

--- task_5.py
+++ task_5.py
@@ -7,14 +7,14 @@ def factorial(n):
 
            silnia *= i
 
-   digits = len(str(silnia))
-   space_1 = 3 - len(str(n))
-   space_2 = 3 - len(str(digits))
-
-
-   print(" " * space_1 + str(n) + "! is " + " " * space_2 + str(digits) + " digits long")
+   return silnia
     
 
 def report():
     for j in range(101):
-        factorial(j)   
+        silnia = factorial(j)
+        digits = len(str(silnia))
+        space_1 = 3 - len(str(j))
+        space_2 = 3 - len(str(digits))
+        print(" " * space_1 + str(j) + "! is " + " " * space_2 + str(digits) + " digits long")
+
